716fca3abae42707249f051edbd2f2c2e6444cf8
misha <mykhaylohud@gmail.com>
2020-09-11 14:04:20 +0300

    Updates

---
+++ precache-manifest.b249423b53337ee90d211b52d3d174e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447b08b1fe349866a41bd9e59ff4cfb2",
+    "url": "/Currency-exchange/index.html"
+  },
+  {
+    "revision": "b4c1b8e56e930a574ae5",
+    "url": "/Currency-exchange/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "061fe7be2787761c69d7",
+    "url": "/Currency-exchange/static/css/main.32044206.chunk.css"
+  },
+  {
+    "revision": "b4c1b8e56e930a574ae5",
+    "url": "/Currency-exchange/static/js/2.74fd9914.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Currency-exchange/static/js/2.74fd9914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061fe7be2787761c69d7",
+    "url": "/Currency-exchange/static/js/main.ed104338.chunk.js"
+  },
+  {
+    "revision": "602f34f82009c25b7f2d",
+    "url": "/Currency-exchange/static/js/runtime-main.b20f999c.js"
+  },
+  {
+    "revision": "2ada898d22a4c12b799993854ba44d61",
+    "url": "/Currency-exchange/static/media/body-bg.2ada898d.png"
+  },
+  {
+    "revision": "67cc5a637feda30f73f7f76eafb5d43b",
+    "url": "/Currency-exchange/static/media/logo3.67cc5a63.png"
+  }
+]);
